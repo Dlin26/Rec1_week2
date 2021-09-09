bd4d8920a1dace56b9ecd73a62a02a2c8ad1a6a8
Dlin26 <90361979+Dlin26@users.noreply.github.com>
2021-09-08 21:18:19 -0400

    Add files via upload

---
+++ Rec1_week2.py
@@ -0,0 +1,25 @@
+x=10.3
+print(x)
+
+
+abcd=100.6
+print(type(abcd))
+
+
+points_obtained=99
+total_points=100
+percentage=((points_obtained/total_points)
+*100)
+print(percentage)
+
+
+my_string='6'
+print(type(my_string))
+
+
+abcd='i am a studen9'
+print(abcd)
+print("-----")
+print(abcd[3])
+print(abcd[-1])
+
